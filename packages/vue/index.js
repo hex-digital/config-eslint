69cc16b3c3381ae152d85b30d1623c95b63c1cba
Jamie Warburton <jamie@hexdigital.com>
2022-08-31 14:27:46 +0100

    feat: ignore vue custom event names that are camelCase with colons e.g. close:click

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -2,4 +2,7 @@ module.exports = {
   extends: [
     '@hexdigital/eslint-config-ts',
   ],
+  rules: {
+    'vue/custom-event-name-casing': ['error', 'camelCase', { ignores: ['/^[a-z]+(?:[A-Z](?:[a-z])*)*:[a-z]+(?:[A-Z](?:[a-z])*)*$/u'] }],
+  },
 }
