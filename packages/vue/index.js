38a8e011e3bcb874a297657b3442cc678551494c
Hipkiss <jonathan@hexdigital.com>
2023-02-17 11:29:58 +0000

    feat: vue max-attributes-per-line

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -4,5 +4,16 @@ module.exports = {
   ],
   rules: {
     'vue/custom-event-name-casing': ['error', 'camelCase', { ignores: ['/^[a-z]+(?:[A-Z](?:[a-z])*)*:[a-z]+(?:[A-Z](?:[a-z])*)*$/u'] }],
+    "vue/max-attributes-per-line": [
+      "error",
+      {
+        "singleline": {
+          "max": 3
+        },      
+        "multiline": {
+          "max": 1
+        }
+      }
+    ],
   },
 }
