0ae9396ed91d9a8a8583acb10b581c558f437c7e
Hipkiss <jonathan@hexdigital.com>
2023-02-17 11:28:47 +0000

    feat: component-tags-order rule

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -4,5 +4,15 @@ module.exports = {
   ],
   rules: {
     'vue/custom-event-name-casing': ['error', 'camelCase', { ignores: ['/^[a-z]+(?:[A-Z](?:[a-z])*)*:[a-z]+(?:[A-Z](?:[a-z])*)*$/u'] }],
+    "vue/component-tags-order": [
+      "error",
+      {
+        "order": [
+          "template",
+          "script",
+          "style"
+        ]
+      }
+    ],
   },
 }
