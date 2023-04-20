9c08d3f7ea35746225feb1117c1e0aefe88a7886
Jamie Warburton <jamie@hexdigital.com>
2023-04-20 15:51:06 +0100

    fix: fix for no-unused-vars not working

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -3,30 +3,30 @@ module.exports = {
     '@hexdigital/eslint-config-ts',
   ],
   rules: {
-    "unused-imports/no-unused-vars": ["warning", {
-      "varsIgnorePattern": "^emit$"
+    'no-unused-vars': ['warn', {
+      varsIgnorePattern: '^emit$',
     }],
-    "vue/custom-event-name-casing": ["error", "camelCase", { ignores: ['/^[a-z]+(?:[A-Z](?:[a-z])*)*:[a-z]+(?:[A-Z](?:[a-z])*)*$/u'] }],
-    "vue/max-attributes-per-line": [
-      "error",
+    'vue/custom-event-name-casing': ['error', 'camelCase', { ignores: ['/^[a-z]+(?:[A-Z](?:[a-z])*)*:[a-z]+(?:[A-Z](?:[a-z])*)*$/u'] }],
+    'vue/max-attributes-per-line': [
+      'error',
       {
-        "singleline": {
-          "max": 3
-        },      
-        "multiline": {
-          "max": 1
-        }
-      }
+        singleline: {
+          max: 3,
+        },
+        multiline: {
+          max: 1,
+        },
+      },
     ],
-    "vue/component-tags-order": [
-      "error",
+    'vue/component-tags-order': [
+      'error',
       {
-        "order": [
-          "template",
-          "script",
-          "style"
-        ]
-      }
+        order: [
+          'template',
+          'script',
+          'style',
+        ],
+      },
     ],
   },
-}
+};
