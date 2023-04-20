53ce91997eb19f9ea22293457394552fc98c208f
Jamie Warburton <jamie@hexdigital.com>
2023-04-20 15:42:57 +0100

    feat: ignore unused "emit" variable, as it's used outside of components

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -3,7 +3,10 @@ module.exports = {
     '@hexdigital/eslint-config-ts',
   ],
   rules: {
-    'vue/custom-event-name-casing': ['error', 'camelCase', { ignores: ['/^[a-z]+(?:[A-Z](?:[a-z])*)*:[a-z]+(?:[A-Z](?:[a-z])*)*$/u'] }],
+    "unused-imports/no-unused-vars": ["warning", {
+      "varsIgnorePattern": "^emit$"
+    }],
+    "vue/custom-event-name-casing": ["error", "camelCase", { ignores: ['/^[a-z]+(?:[A-Z](?:[a-z])*)*:[a-z]+(?:[A-Z](?:[a-z])*)*$/u'] }],
     "vue/max-attributes-per-line": [
       "error",
       {
