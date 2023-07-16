d947b52cc0f6dce3183c1fa2825e13cc2dc61ecf
Jamie Warburton <jamie@hexdigital.com>
2023-07-16 16:52:28 +0100

    feat: override no `process.env` rule

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -2,6 +2,7 @@ module.exports = {
   rules: {
     'semi': ['error', 'always'],
     'jsonc/comma-dangle': ['error', 'always'],
+    'n/prefer-global/process': ['off'], // One day we might turn this on. See: https://github.com/antfu/eslint-config/pull/214
   },
   overrides: [
     {
